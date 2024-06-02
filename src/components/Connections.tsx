468db26e324baca0d2f2aa8db0b4e3b84935e920
tartebabba <83190958+tartebabba@users.noreply.github.com>
2024-06-02 22:59:26 +0100

    added ability to click and unclick items

--- src/components/Connections.tsx
+++ src/components/Connections.tsx
@@ -1,11 +1,62 @@
 import ConnectionsGrid from './connections-grid';
+import { extractNShuffleSongs, shuffleSongs } from './utils';
+import { useState } from 'react';
 
 export default function Connections() {
+  const [detailsForGame, setDetailsForGame] = useState({});
+
+  const songs = extractNShuffleSongs(gridItems);
+  // const onetoSixteen = Array.from({ length: 16 }, (_, i) => i + 1);
+  const shuffledSongs = shuffleSongs(songs);
+
+  const gridComponents = {
+    shuffledSongs,
+  };
+
   return (
     <div>
       <h1>Connections</h1>
       <p>Connections page content</p>
-      <ConnectionsGrid />
+      <ConnectionsGrid {...gridComponents} />
     </div>
   );
 }
+
+const gridItems = [
+  {
+    artists: 'Artist 1',
+    songs: [
+      'Artist 1 - Song 1',
+      'Artist 1 - Song 2',
+      'Artist 1 - Song 3',
+      'Artist 1 - Song 4',
+    ],
+  },
+  {
+    artists: 'Artist 2',
+    songs: [
+      'Artist 2 - Song 1',
+      'Artist 2 - Song 2',
+      'Artist 2 - Song 3',
+      'Artist 2 - Song 4',
+    ],
+  },
+  {
+    artists: 'Artist 3',
+    songs: [
+      'Artist 3 - Song 1',
+      'Artist 3 - Song 2',
+      'Artist 3 - Song 3',
+      'Artist 3 - Song 4',
+    ],
+  },
+  {
+    artists: 'Artist 4',
+    songs: [
+      'Artist 4 - Song 1',
+      'Artist 4 - Song 2',
+      'Artist 4 - Song 3',
+      'Artist 4 - Song 4',
+    ],
+  },
+];
--- src/components/connections-grid.tsx
+++ src/components/connections-grid.tsx
@@ -1,67 +1,71 @@
 import { extractSongs, shuffleSongs } from './utils';
+import { useState } from 'react';
 
-export default function ConnectionsGrid() {
-  const songs = extractSongs(gridItems);
-  const shuffledSongs = shuffleSongs(songs);
-  const gridArea = shuffledSongs;
+interface GameDetails {
+  songsForGrid: string[];
+  selected: string[];
+}
+
+type ConnectionsGridProps = {
+  shuffledSongs: string[];
+};
+
+export default function ConnectionsGrid(props: ConnectionsGridProps) {
+  const { shuffledSongs } = props;
+
+  const [currentGameDetails, setCurrentGameDetails] = useState<GameDetails>({
+    songsForGrid: shuffledSongs,
+    selected: [],
+  });
+
+  console.log(currentGameDetails.selected);
+
+  function handleClick(item) {
+    const selectedSong = item.target.innerText;
+
+    if (currentGameDetails.selected.includes(selectedSong)) {
+      setCurrentGameDetails((prev) => ({
+        ...prev,
+        selected: prev.selected.filter((song) => song !== selectedSong),
+      }));
+    } else if (currentGameDetails.selected.length < 4) {
+      setCurrentGameDetails((prev) => ({
+        ...prev,
+        selected: [...prev.selected, selectedSong],
+      }));
+    }
+  }
+
+  function isSelected(song: string) {
+    return currentGameDetails.selected.includes(song);
+  }
 
   return (
     <>
       <h1>Connections Grid</h1>
       <div className="grid grid-cols-4 grid-rows-4 gap-2">
-        {gridArea.map((item) => (
-          <div
-            key={item}
-            className=" border bg-amber-100 m-2 p-1 flex items-center justify-center aspect-square"
-          >
-            <p>{item}</p>
-          </div>
-        ))}
+        {currentGameDetails.songsForGrid.map((song) => {
+          return (
+            <div
+              key={song}
+              onClick={(x) => handleClick(x)}
+              className={`border ${
+                isSelected(song) ? 'bg-green-500' : 'bg-amber-100'
+              } m-2 p-1 text-black flex items-center justify-center aspect-square`}
+            >
+              <p>{song}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
 }
+
+
 
 // [x] Center Text in Grid
 // [] Add a shuffle button
 // [] Add a reset button
 // [] Colour toggles for clicks on circles
 
-const gridItems = [
-  {
-    artists: 'Artist 1',
-    songs: [
-      'Artist 1 - Song 1',
-      'Artist 1 - Song 2',
-      'Artist 1 - Song 3',
-      'Artist 1 - Song 4',
-    ],
-  },
-  {
-    artists: 'Artist 2',
-    songs: [
-      'Artist 2 - Song 1',
-      'Artist 2 - Song 2',
-      'Artist 2 - Song 3',
-      'Artist 2 - Song 4',
-    ],
-  },
-  {
-    artists: 'Artist 3',
-    songs: [
-      'Artist 3 - Song 1',
-      'Artist 3 - Song 2',
-      'Artist 3 - Song 3',
-      'Artist 3 - Song 4',
-    ],
-  },
-  {
-    artists: 'Artist 4',
-    songs: [
-      'Artist 4 - Song 1',
-      'Artist 4 - Song 2',
-      'Artist 4 - Song 3',
-      'Artist 4 - Song 4',
-    ],
-  },
-];
