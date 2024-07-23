aa423abbeaa88dcb93878c9c69993ad9bf786035
tartebabba <83190958+tartebabba@users.noreply.github.com>
2024-07-23 15:33:44 +0100

    sync

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -2,9 +2,12 @@ import { GearIcon, InfoCircledIcon } from '@radix-ui/react-icons';
 
 export default function InfoBar() {
   return (
-    <div className="flex justify-end py-2 border-b ">
-      <InfoCircledIcon className="mx-2" />
-      <GearIcon className="mx-2" />
+    <div>
+      <div className="flex justify-end py-2 border-b ">
+        <InfoCircledIcon className="mx-2" />
+        <GearIcon className="mx-2" />
+      </div>
+      <h1 className='pt-4'>Create four groups of four!</h1>
     </div>
   );
 }
--- src/components/connections-grid.tsx
+++ src/components/connections-grid.tsx
@@ -44,15 +44,15 @@ export default function ConnectionsGrid(props: ConnectionsGridProps) {
   }
 
   const stylingForGrid = (song: string) => {
-    return `${isSelected(song) ? 'bg-gray-600 text-white' : 'bg-orange-100'} ${getBackgroundColor(
+    return `${isSelected(song) ? 'bg-gray-600 text-white' : 'bg-[#EFEFE6]'} ${getBackgroundColor(
       song
-    )} m-2 p-1 text-black flex items-center justify-center aspect-square font-bold text-md rounded-md`;
+    )} m-1 p-1 text-black flex items-center justify-center aspect-video font-bold text-md rounded-md`;
   };
 
   return (
     <>
       <div className="flex justify-center items-center my-4">
-        <div className="grid grid-cols-4 gap-2">
+        <div className="grid grid-cols-4">
           {currentGameDetails.correctGroups.flat().map((song) => {
             return (
               <div
@@ -60,7 +60,7 @@ export default function ConnectionsGrid(props: ConnectionsGridProps) {
                 onClick={(data) => handleClick(data)}
                 className={stylingForGrid(song)}
               >
-                <p>{song}</p>
+                <p className='py-3'>{song}</p>
               </div>
             );
           })}
@@ -71,7 +71,7 @@ export default function ConnectionsGrid(props: ConnectionsGridProps) {
                 onClick={(data) => handleClick(data)}
                 className={stylingForGrid(song)}
               >
-                <p>{song}</p>
+                <p className='py-3'>{song}</p>
               </div>
             );
           })}
